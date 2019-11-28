9df87b428bacd410d24fccf233f51895529f05b9
marodev <modermatt@tuta.io>
2019-11-28 16:35:43 +0100

    delete codemr

--- codemr/muck/html/html_report_muck/htmlx/modeldata/packagedependency.js
+++
@@ -1,5 +0,0 @@
-var matrix = [[0]];
-var packages = [{
-"name": " ch.usi.si.codelounge", "color": " #3182bd"
-}
-];
--- codemr/muck/html/main_report/htmlx/modeldata/overview_metric_data.js
+++
@@ -1,1 +0,0 @@
-var CodeMR_ProjectMetrics = {"Project Name":"muck", "Number of Highly Problematic Classes":0, "Number of Entities":3, "Number of Problematic Classes":0, "Class Lines of Code":169, "Number of External Packages":10, "Number of Packages":2, "Number of External Entities":34}
