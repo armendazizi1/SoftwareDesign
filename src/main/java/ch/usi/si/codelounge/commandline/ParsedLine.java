e7920c3b1bccc22cceaf654fbf2c9bdd83ca6094
marodev <modermatt@tuta.io>
2019-12-02 20:59:00 +0100

    remove todo

--- src/main/java/ch/usi/si/codelounge/commandline/ParsedLine.java
+++ src/main/java/ch/usi/si/codelounge/commandline/ParsedLine.java
@@ -16,7 +16,6 @@ public class ParsedLine {
     this.helpMsg = helpMsg;
   }
 
-  // TODO: rewrite
   public boolean hasError() {
     return !(StringUtil.isNotEmpty(getFilepath())
         || StringUtil.isNotEmpty(getSheetName())
